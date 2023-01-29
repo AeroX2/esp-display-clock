cd488322a6d40015e8499cbfd34e48297c9d07ec
AeroX2 <james@ridey.email>
2023-01-29 13:46:01 +1100

    First commit that actually works

--- clock/include/display.h
+++ clock/include/display.h
@@ -1,68 +1,61 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
-// uncomment one line to select your MatrixHardware configuration - configuration header needs to be included before <SmartMatrix.h>
-//#include <MatrixHardware_Teensy3_ShieldV4.h>        // SmartLED Shield for Teensy 3 (V4)
-//#include <MatrixHardware_Teensy4_ShieldV5.h>        // SmartLED Shield for Teensy 4 (V5)
-//#include <MatrixHardware_Teensy3_ShieldV1toV3.h>    // SmartMatrix Shield for Teensy 3 V1-V3
-//#include <MatrixHardware_Teensy4_ShieldV4Adapter.h> // Teensy 4 Adapter attached to SmartLED Shield for Teensy 3 (V4)
-//#include <MatrixHardware_ESP32_V0.h>                // This file contains multiple ESP32 hardware configurations, edit the file to define GPIOPINOUT (or add #define GPIOPINOUT with a hardcoded number before this #include)
-#include "MatrixHardware_Custom.h"                  // Copy an existing MatrixHardware file to your Sketch directory, rename, customize, and you can include it like this
-#include <SmartMatrix.h>
+#include "quarter_scan_panel.h" // Virtual Display to re-map co-ordinates such that they draw correctly on a 32x16 1/4 Scan panel 
+#include <Wire.h>
+#include <FastLED.h>
 
-#define COLOR_DEPTH 24                  // Choose the color depth used for storing pixels in the layers: 24 or 48 (24 is good for most sketches - If the sketch uses type `rgb24` directly, COLOR_DEPTH must be 24)
-const uint16_t kMatrixWidth = 64;       // Set to the width of your display, must be a multiple of 8
-const uint16_t kMatrixHeight = 8;      // Set to the height of your display
-const uint8_t kRefreshDepth = 36;       // Tradeoff of color quality vs refresh rate, max brightness, and RAM usage.  36 is typically good, drop down to 24 if you need to.  On Teensy, multiples of 3, up to 48: 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48.  On ESP32: 24, 36, 48
-const uint8_t kDmaBufferRows = 4;       // known working: 2-4, use 2 to save RAM, more to keep from dropping frames and automatically lowering refresh rate.  (This isn't used on ESP32, leave as default)
-const uint8_t kPanelType = SM_PANELTYPE_HUB75_8ROW_MOD4SCAN;   // Choose the configuration that matches your panels.  See more details in MatrixCommonHub75.h and the docs: https://github.com/pixelmatix/SmartMatrix/wiki
-const uint32_t kMatrixOptions = (SM_HUB75_OPTIONS_NONE);        // see docs for options: https://github.com/pixelmatix/SmartMatrix/wiki
-const uint8_t kBackgroundLayerOptions = (SM_BACKGROUND_OPTIONS_NONE);
-const uint8_t kScrollingLayerOptions = (SM_SCROLLING_OPTIONS_NONE);
-const uint8_t kIndexedLayerOptions = (SM_INDEXED_OPTIONS_NONE);
+// uncomment to use custom pins, then provide below
+#define USE_CUSTOM_PINS 
 
-SMARTMATRIX_ALLOCATE_BUFFERS(matrix, kMatrixWidth, kMatrixHeight, kRefreshDepth, kDmaBufferRows, kPanelType, kMatrixOptions);
-SMARTMATRIX_ALLOCATE_BACKGROUND_LAYER(backgroundLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kBackgroundLayerOptions);
-SMARTMATRIX_ALLOCATE_SCROLLING_LAYER(scrollingLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kScrollingLayerOptions);
-SMARTMATRIX_ALLOCATE_INDEXED_LAYER(indexedLayer, kMatrixWidth, kMatrixHeight, COLOR_DEPTH, kIndexedLayerOptions);
+/* Pin 1,3,5,7,9,11,13,15 */
+#define R1_PIN  16
+#define B1_PIN  17
+#define R2_PIN  5
+#define B2_PIN  18
+#define A_PIN  19
+#define C_PIN  21
+#define CLK_PIN  23
+#define OE_PIN  33
 
-#define MATRIX_WIDTH 32
-#define MATRIX_HEIGHT 16
+/* Pin 2,6,10,12,14 */
+#define G1_PIN  4
+#define G2_PIN  26
+#define B_PIN  15
+#define D_PIN  13
+#define LAT_PIN  25
+#define E_PIN  -1 // required for 1/32 scan panels
 
-const int defaultBrightness = (15*255)/100;
+MatrixPanel_I2S_DMA dmaOutput;
+
+// Create virtual 1/2 to 1/4 scan pixel co-ordinate mapping class.
+QuarterScanMatrixPanel display(dmaOutput);
+
+bool displayEnabled = true;
+
+uint16_t BLUE = display.color565(0, 0, 255);
+uint16_t WHITE = display.color565(255, 0, 0);
+uint16_t BLACK = display.color565(0, 0, 0);
 
 void displayInit() {
-  // display.begin(4);
-  // display.setScanPattern(ZAGZIG);
-  // displayUpdateEnable(true);
+  dmaOutput.begin(R1_PIN, G1_PIN, B1_PIN, R2_PIN, G2_PIN, B2_PIN, A_PIN, B_PIN, C_PIN, D_PIN, E_PIN, LAT_PIN, OE_PIN, CLK_PIN );  // setup the LED matrix
+  dmaOutput.setLatBlanking(2);
 
-  matrix.addLayer(&indexedLayer);
-  matrix.begin();
-
-  // matrix.setBrightness(defaultBrightness);
-
-  // indexedLayer.fillScreen(0);
-  // indexedLayer.setFont(gohufont11b);
-  // indexedLayer.drawString(0, kMatrixHeight / 2 - 6, 1, "CLOCK");
-  // indexedLayer.swapBuffers(false);
+  display.fillScreen(display.color444(0, 0, 0));
 }
 
 void displayUpdate() {
-  // indexedLayer.drawString(0, kMatrixHeight / 2 - 6, 1, "CLOCK");
-  // indexedLayer.swapBuffers();
-      backgroundLayer.fillScreen( {0x40, 0, 0});
-    backgroundLayer.swapBuffers();
+  display.fillScreen(display.color444(0, 0, 0));
+  display.setTextFGColor(display.color565(0,60,255));
 
-    scrollingLayer.setColor({0xff, 0xff, 0xff});
-    scrollingLayer.setMode(wrapForward);
-    scrollingLayer.setSpeed(40);
-    scrollingLayer.setFont(font6x10);
-    scrollingLayer.start("SmartMatrix Demo", 1);
+  display.setCursor(0,5);
+  display.write("HURRA");
+  // delay(timeout);
 }
 
-// void setPixel(int ax, int ay, bool d) {
-//   display.drawPixel(ax, ay, d ? WHITE : BLACK);
-// }
+void setPixel(int ax, int ay, bool d) {
+  display.drawPixel(ax, ay, d ? WHITE : BLACK);
+}
 
 // boolean dec(int number, int x, int y) {
 //   return (NUMBERS_FONT[number][y] >> (7-x)) & 1;
--- clock/src/main.cpp
+++ clock/src/main.cpp
@@ -1,15 +1,14 @@
 #include <Arduino.h>
+#include <WiFiManager.h>
 
-// #include <WiFiManager.h>
-// #include <AsyncTCP.h>
-// #include <ESPAsyncWebServer.h>
-// #include <AsyncElegantOTA.h>
-// #include <time.h>
+// This has to go after WifiManager to prevent HTTP conflicts
+#include <AsyncElegantOTA.h>
+#include <time.h>
 
-#include "font.h"
 #include "display.h"
+#include "font.h"
 
-// AsyncWebServer server(80);
+AsyncWebServer server(80);
 
 // //const char* DAYS[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 // const char* DAYS[] = {"sun", "mon", "tue", "wed", "thu", "fri", "sat"};
@@ -33,7 +32,7 @@
 
 //     if (timeinfo.tm_hour/10 != 0) drawDigit(timeinfo.tm_hour/10,0,-1);
 //     else drawDigit(timeinfo.tm_hour/10,0,-1,true,true);
-    
+
 //     drawDigit(timeinfo.tm_hour%10,7,-1);
 
 //     drawDigit(timeinfo.tm_min/10,17,-1);
@@ -51,7 +50,7 @@
 //     drawChar(10+26+26,21,10);
 //     drawChar(timeinfo.tm_mon/10,24,10);
 //     drawChar(timeinfo.tm_mon%10,28,10);
-    
+
 //     previousTime = timeinfo;
 //   }
 
@@ -64,44 +63,42 @@
 //   drawDigit(10,12,-1,false,showCol);
 // }
 
-boolean otaStarted = false;
 void setup() {
   Serial.begin(115200);
 
-  // // Setup wifi
-  // WiFi.hostname("james_clock_controller");
-  // WiFiManager wifiManager;
-  // wifiManager.autoConnect();
-  
-  // server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-  //   request->send(200, "text/plain", "Hi! I am ESP32.");
-  // });
+  // Setup wifi
+  WiFi.hostname("james_clock_controller");
+  WiFiManager wifiManager;
+  wifiManager.autoConnect();
 
-  // // Bypass ElegantOTA so that it doesn't crash with the display timer.
-  // server.on("/update/identity", HTTP_GET, [&](AsyncWebServerRequest *request) {
-  //   #if defined(ESP8266)
-  //     request->send(200, "application/json", "{\"id\": \"clock\", \"hardware\": \"ESP8266\"}");
-  //   #elif defined(ESP32)
-  //     request->send(200, "application/json", "{\"id\": \"clock\", \"hardware\": \"ESP32\"}");
-  //   #endif
-  // });
-  // server.on("/update", HTTP_GET, [&](AsyncWebServerRequest *request){
-  //   Serial.println("Cancel display update");
-  //   displayUpdateEnable(false);
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Hi! I am ESP32.");
+  });
 
-  //   AsyncWebServerResponse *response = request->beginResponse_P(200, "text/html", ELEGANT_HTML, ELEGANT_HTML_SIZE);
-  //   response->addHeader("Content-Encoding", "gzip");
-  //   request->send(response);
-  // });
-  
-  // AsyncElegantOTA.begin(&server);
-  // server.begin();
-  // Serial.println("HTTP server started");
-  
-  // configTime(0, 0, "pool.ntp.org", "time.nist.gov");
-  // setenv("TZ", "AEST-10AEDT,M10.1.0,M4.1.0/3", 0); // https://github.com/nayarsystems/posix_tz_db
-  
-	// Init the display
+  // Bypass ElegantOTA so that it doesn't crash with the display timer.
+  server.on("/update/identity", HTTP_GET, [&](AsyncWebServerRequest *request) {
+    request->send(200, "application/json",
+                  "{\"id\": \"clock\", \"hardware\": \"ESP32\"}");
+  });
+  server.on("/update", HTTP_GET, [&](AsyncWebServerRequest *request) {
+    // Wifi is really unresponsive if the display is still spitting out signals.
+    dmaOutput.stopDMAoutput();
+
+    AsyncWebServerResponse *response = request->beginResponse_P(
+        200, "text/html", ELEGANT_HTML, ELEGANT_HTML_SIZE);
+    response->addHeader("Content-Encoding", "gzip");
+    request->send(response);
+  });
+
+  AsyncElegantOTA.begin(&server);
+  server.begin();
+  Serial.println("HTTP server started");
+
+  configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+  setenv("TZ", "AEST-10AEDT,M10.1.0,M4.1.0/3",
+         0);  // https://github.com/nayarsystems/posix_tz_db
+
+  // Init the display
   displayInit();
 
   Serial.println("Ready");
@@ -109,8 +106,39 @@ void setup() {
   // Serial.println(WiFi.localIP());
 }
 
+int time_counter = 0;
 void loop() {
-  // updateTime();
-  displayUpdate();
+  // if (displayEnabled) {
+  // delay(5000);
+  // // for (int y = 0; y < 16; y++) {
+  // //   for (int x = 0; x < 32; x++) {
+  // //     setPixel(x, y, 1);
+  // //     delay(50);
+  // //   }
+  // // }
+  // // display.fillScreen(display.color444(0, 0, 0));
+  // // updateTime();
+  // displayUpdate();
+  // }
+
+  for (int x = 0; x < 32; x++) {
+    for (int y = 0; y < 16; y++) {
+      int16_t v = 0;
+      uint8_t wibble = sin8(time_counter);
+      v += sin16(x * wibble * 6 + time_counter);
+      v += cos16(y * (128 - wibble) + time_counter);
+      v += sin16(y * x * cos8(-time_counter) / 8);
+
+      CRGB currentColor = ColorFromPalette(RainbowColors_p, (v >> 8) + 127);
+      display.drawPixelRGB888(x, y, currentColor.r, currentColor.g,
+                              currentColor.b);
+    }
+  }
+
+  display.setTextColor(0);
+  display.setCursor(0,5);
+  display.write("12:54 PM");
+
+  time_counter++;
   delay(50);
 }
