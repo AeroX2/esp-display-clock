e8906250ec896b736d05ed1b359142af59d6ae0a
AeroX2 <james@ridey.email>
2023-01-29 21:36:54 +1100

    Upgrade to latest MatrixPanel library, add time, remote debugging

--- clock/include/display.h
+++ clock/include/display.h
@@ -1,91 +1,40 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
-#include "quarter_scan_panel.h" // Virtual Display to re-map co-ordinates such that they draw correctly on a 32x16 1/4 Scan panel 
-#include <Wire.h>
-#include <FastLED.h>
-
-// uncomment to use custom pins, then provide below
-#define USE_CUSTOM_PINS 
+#include "quarter_scan_panel.h"  // Virtual Display to re-map co-ordinates such that they draw correctly on a 32x16 1/4 Scan panel
 
 /* Pin 1,3,5,7,9,11,13,15 */
-#define R1_PIN  16
-#define B1_PIN  17
-#define R2_PIN  5
-#define B2_PIN  18
-#define A_PIN  19
-#define C_PIN  21
-#define CLK_PIN  23
-#define OE_PIN  33
+#define R1_PIN 16
+#define B1_PIN 17
+#define R2_PIN 5
+#define B2_PIN 18
+#define A_PIN 19
+#define C_PIN 21
+#define CLK_PIN 23
+#define OE_PIN 33
 
 /* Pin 2,6,10,12,14 */
-#define G1_PIN  4
-#define G2_PIN  26
-#define B_PIN  15
-#define D_PIN  13
-#define LAT_PIN  25
-#define E_PIN  -1 // required for 1/32 scan panels
+#define G1_PIN 4
+#define G2_PIN 26
+#define B_PIN 15
+#define D_PIN 13
+#define LAT_PIN 25
+#define E_PIN -1  // required for 1/32 scan panels
 
-MatrixPanel_I2S_DMA dmaOutput;
+HUB75_I2S_CFG mxconfig(64, 8, 1,
+                       {R1_PIN, G1_PIN, B1_PIN, R2_PIN, G2_PIN, B2_PIN, A_PIN,
+                        B_PIN, C_PIN, D_PIN, E_PIN, LAT_PIN, OE_PIN, CLK_PIN},
+                       HUB75_I2S_CFG::SHIFTREG, false, HUB75_I2S_CFG::HZ_15M, 3,
+                       true);
 
-// Create virtual 1/2 to 1/4 scan pixel co-ordinate mapping class.
-QuarterScanMatrixPanel display(dmaOutput);
+MatrixPanel_I2S_DMA dmaOutput(mxconfig);
 
-bool displayEnabled = true;
-
-uint16_t BLUE = display.color565(0, 0, 255);
-uint16_t WHITE = display.color565(255, 0, 0);
-uint16_t BLACK = display.color565(0, 0, 0);
+// // Create virtual 1/2 to 1/4 scan pixel co-ordinate mapping class.
+QuarterScanMatrixPanel display(dmaOutput, 1, 1, 32, 16);
 
 void displayInit() {
-  dmaOutput.begin(R1_PIN, G1_PIN, B1_PIN, R2_PIN, G2_PIN, B2_PIN, A_PIN, B_PIN, C_PIN, D_PIN, E_PIN, LAT_PIN, OE_PIN, CLK_PIN );  // setup the LED matrix
-  dmaOutput.setLatBlanking(2);
-
-  display.fillScreen(display.color444(0, 0, 0));
+  dmaOutput.begin();
+  display.fillScreen(0);
 }
-
-void displayUpdate() {
-  display.fillScreen(display.color444(0, 0, 0));
-  display.setTextFGColor(display.color565(0,60,255));
-
-  display.setCursor(0,5);
-  display.write("HURRA");
-  // delay(timeout);
-}
-
-void setPixel(int ax, int ay, bool d) {
-  display.drawPixel(ax, ay, d ? WHITE : BLACK);
-}
-
-// boolean dec(int number, int x, int y) {
-//   return (NUMBERS_FONT[number][y] >> (7-x)) & 1;
-// }
-
-// boolean decc(int chr, int x,int y) {
-//   return (TEXT_FONT[chr][x] >> (y)) & 1;
-// }
-
-// void drawDigit(int number, 
-//                int ox, int oy, 
-//                bool update = true,
-//                bool clear = false) {
-//   for (int y = 0; y < NUM_HEIGHT; y++) {
-//     for (int x = 0; x < NUM_WIDTH; x++) {
-//       bool d = dec(number,x,y);
-//       if (d || update) {
-//         if (clear) d = false;
-//         setPixel(ox+x,oy+y,d);
-//       }
-//     }
-//   }
-// }
-
-// void drawChar(char chr, int ox, int oy) {
-//   for (int y = 0; y < TEXT_HEIGHT; y++) {
-//     for (int x = 0; x < TEXT_WIDTH; x++) {
-//       setPixel(ox+x,oy+y,decc(chr,x,y));
-//     }
-//   }
-// }
 
 #endif
--- clock/src/main.cpp
+++ clock/src/main.cpp
@@ -1,67 +1,94 @@
 #include <Arduino.h>
+#include <FastLED.h>
 #include <WiFiManager.h>
 
 // This has to go after WifiManager to prevent HTTP conflicts
 #include <AsyncElegantOTA.h>
 #include <time.h>
 
+// Goes before display.h incase we want remote debugging there.
+#include <RemoteDebug.h>
+RemoteDebug Debug;
+
 #include "display.h"
-#include "font.h"
 
 AsyncWebServer server(80);
 
-// //const char* DAYS[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-// const char* DAYS[] = {"sun", "mon", "tue", "wed", "thu", "fri", "sat"};
+char currentTime[10] = {0};
+char currentTimeNoColumn[10] = {0};
+char currentDate[10] = {0};
+struct tm previousTime = {0};
 
-// bool transistion = false;
+bool showCol = false;
+unsigned long prevColTime = 0;
 
-// bool showCol = false;
-// unsigned long prevColTime = 0;
-// unsigned long prevTimeUpdate = 0;
-// struct tm previousTime;
-// void updateTime() {
-//   struct tm timeinfo;
-//   if (!getLocalTime(&timeinfo)) {
-//     Serial.println("Failed to obtain time");
-//     return;
-//   }
+void updateTime() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    // debugD("Failed to obtain time");
+    return;
+  }
 
-//   // Draw the new time
-//   if (previousTime.tm_min != timeinfo.tm_min) {
-//     Serial.println("Update the time");
+  // Blick column
+  unsigned long currTime = millis();
+  if (currTime - prevColTime > 500) {
+    showCol = !showCol;
+    prevColTime = currTime;
+  }
 
-//     if (timeinfo.tm_hour/10 != 0) drawDigit(timeinfo.tm_hour/10,0,-1);
-//     else drawDigit(timeinfo.tm_hour/10,0,-1,true,true);
+  // Draw the new time
+  if (previousTime.tm_min != timeinfo.tm_min) {
+    // debugD("Update the time");
 
-//     drawDigit(timeinfo.tm_hour%10,7,-1);
+    strftime(currentTime, sizeof(currentTime), "%k:%M", &timeinfo);
+    if (currentTime[0] == ' ') strcpy(currentTime, &currentTime[1]);
+    strftime(currentTimeNoColumn, sizeof(currentTimeNoColumn), "%k %M",
+             &timeinfo);
+    if (currentTimeNoColumn[0] == ' ')
+      strcpy(currentTimeNoColumn, &currentTimeNoColumn[1]);
+    strftime(currentDate, sizeof(currentDate), "%b%e", &timeinfo);
 
-//     drawDigit(timeinfo.tm_min/10,17,-1);
-//     drawDigit(timeinfo.tm_min%10,24,-1);
+    previousTime = timeinfo;
+  }
+}
 
-//     // Draw the new date
-//     const char* day = DAYS[timeinfo.tm_wday];
+void drawCenteredString(const char *buf, int x, int y) {
+  int16_t x1, y1;
+  uint16_t w, h;
+  display.getTextBounds(buf, x, y, &x1, &y1, &w,
+                        &h);  // calc width of new string
+  display.setCursor(x - w / 2 - 1, y);
+  display.write(buf);
+}
 
-//     drawChar(day[0]-'a'+10+26,0,10);
-//     drawChar(day[1]-'a'+10,4,10);
-//     drawChar(day[2]-'a'+10,8,10);
+unsigned int waveTimeCounter = 0;
+void displayUpdate() {
+  updateTime();
 
-//     drawChar(timeinfo.tm_mday/10,13,10);
-//     drawChar(timeinfo.tm_mday%10,17,10);
-//     drawChar(10+26+26,21,10);
-//     drawChar(timeinfo.tm_mon/10,24,10);
-//     drawChar(timeinfo.tm_mon%10,28,10);
+  display.fillScreen(0);
 
-//     previousTime = timeinfo;
-//   }
+  display.setTextColor(display.color565(0, 0, 8));
+  drawCenteredString(showCol ? currentTime : currentTimeNoColumn, 32 / 2 - 1,
+                     1);
+  drawCenteredString(currentDate, 32 / 2 - 1, 9);
 
-//   // Blick column
-//   unsigned long currTime = millis();
-//   if (currTime - prevColTime > 500) {
-//     showCol = !showCol;
-//     prevColTime = currTime;
-//   }
-//   drawDigit(10,12,-1,false,showCol);
-// }
+  for (int x = 0; x < 32; x++) {
+    for (int y = 0; y < 16; y++) {
+      if (display.getPixel(x, y) > 0) continue;
+
+      int16_t v = 0;
+      uint8_t wibble = sin8(waveTimeCounter);
+      v += sin16(x * wibble * 6 + waveTimeCounter);
+      v += cos16(y * (128 - wibble) + waveTimeCounter);
+      v += sin16(y * x * cos8(-waveTimeCounter) / 8);
+
+      CRGB currentColor = ColorFromPalette(RainbowColors_p, (v >> 8) + 127);
+      display.drawPixelRGB888(x, y, currentColor.r, currentColor.g,
+                              currentColor.b);
+    }
+  }
+  waveTimeCounter++;
+}
 
 void setup() {
   Serial.begin(115200);
@@ -92,53 +119,34 @@ void setup() {
 
   AsyncElegantOTA.begin(&server);
   server.begin();
-  Serial.println("HTTP server started");
 
   configTime(0, 0, "pool.ntp.org", "time.nist.gov");
   setenv("TZ", "AEST-10AEDT,M10.1.0,M4.1.0/3",
-         0);  // https://github.com/nayarsystems/posix_tz_db
+         1);  // https://github.com/nayarsystems/posix_tz_db
+  tzset();
 
   // Init the display
   displayInit();
 
-  Serial.println("Ready");
-  // Serial.print("IP address: ");
-  // Serial.println(WiFi.localIP());
+  Serial.println("Waiting for Wifi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+  }
+  Serial.println("Connected!");
+
+  Debug.begin("james_clock_controller");
+  debugD("Ready!");
+
+  delay(3000);
 }
 
-int time_counter = 0;
+int updateTimeCounter = 0;
 void loop() {
-  // if (displayEnabled) {
-  // delay(5000);
-  // // for (int y = 0; y < 16; y++) {
-  // //   for (int x = 0; x < 32; x++) {
-  // //     setPixel(x, y, 1);
-  // //     delay(50);
-  // //   }
-  // // }
-  // // display.fillScreen(display.color444(0, 0, 0));
-  // // updateTime();
-  // displayUpdate();
-  // }
-
-  for (int x = 0; x < 32; x++) {
-    for (int y = 0; y < 16; y++) {
-      int16_t v = 0;
-      uint8_t wibble = sin8(time_counter);
-      v += sin16(x * wibble * 6 + time_counter);
-      v += cos16(y * (128 - wibble) + time_counter);
-      v += sin16(y * x * cos8(-time_counter) / 8);
-
-      CRGB currentColor = ColorFromPalette(RainbowColors_p, (v >> 8) + 127);
-      display.drawPixelRGB888(x, y, currentColor.r, currentColor.g,
-                              currentColor.b);
-    }
+  if (updateTimeCounter++ > 50) {
+    updateTimeCounter = 0;
+    displayUpdate();
   }
 
-  display.setTextColor(0);
-  display.setCursor(0,5);
-  display.write("12:54 PM");
-
-  time_counter++;
-  delay(50);
+  Debug.handle();
+  delay(1);
 }
